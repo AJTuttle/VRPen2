5aeecb101bc6e5ebcc45509d5ce5bcb70540e68f
AJ Tuttle <alexander.tuttle@uga.edu>
2019-09-28 19:24:49 -0400

    made ui a prefab

--- VRPen2/Assets/Scripts/Vector/VectorLine.cs
+++ VRPen2/Assets/Scripts/Vector/VectorLine.cs
@@ -17,10 +17,10 @@ namespace VRPen {
 
         //drawpoints
         public int pointCount;
-        public Vector3[] normals;
-        public Vector3[] vertices;
-        public List<int> indices;
+        public Vector3[] normals;    //array so we only need to copy every x items (if we increase the size by x each time)
+        public Vector3[] vertices;   //array so we only need to copy every x items (if we increase the size by x each time)
+        public List<int> indices;    //list since we cant have empty values like we can for normals or verts
 
-  
+
     }
 }
