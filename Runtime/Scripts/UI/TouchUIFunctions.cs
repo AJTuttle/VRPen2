dfda904c38d1ca6ba91a01f107d10177e1fb741f
AJ Tuttle <alexander.tuttle@uga.edu>
2020-04-25 17:24:13 -0400

    Added scaling to touch canvases, added icon to project, made other small changes

---
+++ Runtime/Scripts/UI/TouchUIFunctions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+namespace VRPen {
+
+
+    //this is supplemental ui functions specifically for use with the touch use-case (phone/tablet)
+    public class TouchUIFunctions : MonoBehaviour {
+
+        public GameObject displayObj;
+
+        void Start() {
+
+        }
+        
+        void Update() {
+
+        }
+
+        public void changeCanvasSize(bool increase) {
+            float increment = increase ? 0.1f : -0.1f;
+            displayObj.transform.localScale += new Vector3(increment, increment, increment);
+        }
+    }
+
+}
