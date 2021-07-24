7664cb6b4df0574c51a1b8e42fd8dbd2b545de83
AJTuttle <alexander.tuttle@uga.edu>
2021-07-24 17:26:22 -0400

    Added remote tablet

---
+++ Runtime/Scripts/Drawing interfaces/Tablet/RemoteTablet.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRPen;
+
+public class RemoteTablet : InputVisuals {
+    
+    //add tablet specific stuff here
+    
+}
