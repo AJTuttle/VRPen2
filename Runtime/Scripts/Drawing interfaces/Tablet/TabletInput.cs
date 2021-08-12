71608083e7c7e380a876e4515ac836c3aa1ba35f
AJTuttle <alexander.tuttle@uga.edu>
2021-08-12 15:54:47 -0400

    quick fix for bug in last commit (the x cursor over no canvas thing)

--- Runtime/Scripts/Drawing interfaces/Tablet/TabletInput.cs
+++ Runtime/Scripts/Drawing interfaces/Tablet/TabletInput.cs
@@ -305,7 +305,7 @@ namespace VRPen {
             
             //set cursor 
             if (data.hover == HoverState.DRAW) {
-                if (data.display.currentLocalCanvas != null) setCursorMesh(0);
+                if (data.display != null && data.display.currentLocalCanvas != null) setCursorMesh(0);
                 else setCursorMesh(2);
             }
             else if (data.hover == HoverState.PALETTE || data.hover == HoverState.GRABBABLE ||
