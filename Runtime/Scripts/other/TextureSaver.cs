d98ee396f25a30a8d10a1be031881de2c16d30d3
AJTuttle <alexander.tuttle@uga.edu>
2021-11-21 18:29:06 -0500

    made canvas exports be 32 bit not 24

--- Runtime/Scripts/other/TextureSaver.cs
+++ Runtime/Scripts/other/TextureSaver.cs
@@ -47,7 +47,7 @@ namespace VRPen {
         
 		public static void export(RenderTexture rt, string name) {
 
-			Texture2D tex = new Texture2D(rt.width, rt.height, TextureFormat.RGB24, false);
+			Texture2D tex = new Texture2D(rt.width, rt.height, TextureFormat.RGBA32, false);
 			RenderTexture.active = rt;
 			tex.ReadPixels(new Rect(0, 0, rt.width, rt.height), 0, 0);
 			tex.Apply();
